ea51d1f3e7d2fb5e70c7c09f5d63ea9f0c9846ae
zhongyi <576822974@qq.com>
2022-11-14 15:03:59 +0800

    feat: loader

---
+++ src/loaders/my-loader.js
@@ -0,0 +1,5 @@
+module.exports = function (source) {
+  // 处理 source ...
+  console.log('(╯°Д°)╯︵ ┻━┻ ', source);
+  return '';
+};
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,17 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
+  // mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.zy$/,
+        use: ['my-loader'], // 处理顺序 html-minify-loader => html-loader => webpack
+      },
+    ],
+  },
+  resolveLoader: {
+    modules: [path.join(__dirname, './src/loaders'), 'node_modules'],
+  },
 };
